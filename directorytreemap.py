a40d5da41cfc11be047eabdf52d833960ae812cd
antonioepoibo <cimino.antonio@gmail.com>
2022-11-02 12:34:12 +0100

    _get e _set

--- directorytreemap.py
+++ directorytreemap.py
@@ -1,6 +1,6 @@
 from hashlib import new
 from directorytree import DirectoryTree
-from .TdP_collections.map import MapBase
+from TdP_collections.map.map_base import MapBase
 
 class DirectoryTreeMap(DirectoryTree,MapBase):
     #---------------------------- override Position class ----------------------------
@@ -17,7 +17,7 @@ class DirectoryTreeMap(DirectoryTree,MapBase):
     def _dicotomic_search(self,p,k,i,f):
         m=(i+f)/2
         if(f<i):
-            return None
+            return p
         if(k.startswith(self.index_children(p,m).key())):
             return self.index_children(p,m)
         if(k<self.index_children(p,m)):
@@ -31,17 +31,40 @@ class DirectoryTreeMap(DirectoryTree,MapBase):
             return p  
         if(k.startswith(p.key())):
             child=self._dicotomic_search(p,k,0,self.num_children(p)-1)
-            if child is not None:
+            if child!=p:
                 return self._subtree_search(child,k)
-        return None
+        return p
         #attenzione devi lavorare sulle position devi mettere la make position 
     
-
-
     def find_position(self,k):
         if self.is_empty():
             return None
         else:
             p = self._subtree_search(self.root(), k)
             return p
+
+    def __getitem__(self, k):
+        """Return value associated with key k (raise KeyError if not found)."""
+        if self.is_empty():
+            raise KeyError('Key Error: ' + repr(k))
+        else:
+            p = self._subtree_search(self.root(), k)
+        if k != p.key():
+            raise KeyError('Key Error: ' + repr(k))
+        return p.value()
+
+    def __setitem__(self, k, v):
+        """Assign value v to key k, overwriting existing value if present."""
+        if self.is_empty():
+            leaf = self._add_root(self._Item(k,v))     # from LinkedBinaryTree
+        else:
+            p = self._subtree_search(self.root(), k)
+            if p.key() == k:
+                p.element()._value = v                   # replace existing item's value
+                return
+            else:
+                item = self._Item(k,v)
+                self._add_child(p,item)
+                
+
 
