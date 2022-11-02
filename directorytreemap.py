073876209f04ce686fdebc0ca73d2b7e94d97533
DOMENICO PIO D'AMORA <d.damora3@studenti.unisa.it>
2022-11-02 12:56:58 +0100

    Aggiustamento identazione in directorytreemap

--- directorytreemap.py
+++ directorytreemap.py
@@ -13,35 +13,35 @@ class DirectoryTreeMap(DirectoryTree,MapBase):
             """Return value of map's key-value pair."""
             return self.element()._value
 
-        #------------------------------- nonpublic utilities -------------------------------
-        def _dicotomic_search(self,p,k,i,f):
-            m=(i+f)/2
-            if(f<i):
-                return p
-            if(k.startswith(self.index_children(p,m).key())):
-                return self.index_children(p,m)
-            if(k<self.index_children(p,m)):
-                return self._dicotomic_search(p,k,i,m-1)
-            if(k>self.index_children(p,m)):
-                return self._dicotomic_search(p,k,m+1,f)
-            
-        def _subtree_search(self, p, k):
-            """Return Position of p's subtree having key k, or last node searched."""
-            if k == p.key():                                   # found match
-                return p  
-            if(k.startswith(p.key())):
-                child=self._dicotomic_search(p,k,0,self.num_children(p)-1)
-                if child!=p:
-                    return self._subtree_search(child,k)
+    #------------------------------- nonpublic utilities -------------------------------
+    def _dicotomic_search(self,p,k,i,f):
+        m=(i+f)/2
+        if(f<i):
             return p
-            #attenzione devi lavorare sulle position devi mettere la make position 
+        if(k.startswith(self.index_children(p,m).key())):
+            return self.index_children(p,m)
+        if(k<self.index_children(p,m)):
+            return self._dicotomic_search(p,k,i,m-1)
+        if(k>self.index_children(p,m)):
+            return self._dicotomic_search(p,k,m+1,f)
         
-        def find_position(self,k):
-            if self.is_empty():
-                return None
-            else:
-                p = self._subtree_search(self.root(), k)
-                return p
+    def _subtree_search(self, p, k):
+        """Return Position of p's subtree having key k, or last node searched."""
+        if k == p.key():                                   # found match
+            return p  
+        if(k.startswith(p.key())):
+            child=self._dicotomic_search(p,k,0,self.num_children(p)-1)
+            if child!=p:
+                return self._subtree_search(child,k)
+        return p
+        #attenzione devi lavorare sulle position devi mettere la make position 
+    
+    def find_position(self,k):
+        if self.is_empty():
+            return None
+        else:
+            p = self._subtree_search(self.root(), k)
+            return p
 
     def __getitem__(self, k):
         """Return value associated with key k (raise KeyError if not found)."""
